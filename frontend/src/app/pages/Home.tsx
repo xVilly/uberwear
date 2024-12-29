25988042f7e666dbff88c64f39924a7882240076
villy-zenbook <mich038al@gmail.com>
2024-12-29 22:17:53 +0100

    tlo na home page

--- frontend/src/app/pages/Home.tsx
+++ frontend/src/app/pages/Home.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import backgroundImage from '../../images/main_site_background.jpg'; // Adjust the relative path
 
 export function HomePage() {
-  return (
-    <div>
-      <h1 className="text-white text-5xl font-bold flex justify-center items-center h-full">
-        Placeholder dla HTMLA strony głównej
-      </h1>
-    </div>
-  );
-}
+    return (
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '100vh',
+          width: '100vw',
+          marginTop: '-4rem',
+        }}
+      >
+        <h1 className="text-white text-5xl font-bold flex justify-center items-center h-full">
+          Placeholder dla HTMLA strony głównej
+        </h1>
+      </div>
+    );
+  }
