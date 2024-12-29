a80649a5fb912122b24b81f7d4eb3d62e1158f6f
wojtekwasek <wojtek.wasek311@gmail.com>
2024-12-29 22:13:42 +0100

    proba dodania tla xd

--- frontend/src/app/pages/Home.tsx
+++ frontend/src/app/pages/Home.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import backgroundImage from '../../images/main_site_background.jpg'; // Adjust the relative path
+
 export function HomePage() {
-    return (
-        <div className="page-container">
-            <h1>Placeholder dla HTMLA strony głównej</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-white text-5xl font-bold flex justify-center items-center h-full">
+        Placeholder dla HTMLA strony głównej
+      </h1>
+    </div>
+  );
 }
