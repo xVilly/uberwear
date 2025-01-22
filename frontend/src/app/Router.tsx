b4c84562948056104799b371bce4e58b7ee08d99
bosok-maker <karolbasak383@gmail.com>
2025-01-23 00:45:16 +0100

    working

--- frontend/src/app/Router.tsx
+++ frontend/src/app/Router.tsx
@@ -6,23 +6,16 @@ import { CreateAccountPage } from "./pages/CreateAccount";
 import AccountPageOrders from "./pages/AccountPageOrders";
 import AccountPagePoints  from "./pages/AccountPagePoints";
 import AccountPageFavBrands  from "./pages/AccountPageFavBrands";
-import { OfferPage } from "./pages/Offer";
 import { CartPage } from "./pages/Cart";
 import AdminCouriers from "./pages/admin/AdminCouriers";
 import AdminOrders from "./pages/admin/AdminOrders";
 import AdminClients from "./pages/admin/AdminClients";
-import { FashionBout } from "./pages/ShopFashionBout";
-import { NYClothes } from "./pages/ShopNyClothes";
-import { NYHoodies } from "./pages/NYClothesHoodies";
-import { NYSweatPants } from "./pages/NYClothesSweatPants";
-import { NYTees } from "./pages/NYClothesTees";
+
 import { AboutPage } from "./pages/AboutPage";
 import { ContactPage } from "./pages/ContactPage";
 import { RegisterShopPage } from "./pages/RegisterShopPage";
 import { CourierPanelPage } from "./pages/CourierPanel";
-import { PurchasePageNYH } from './pages/NYHoodiePurchasePage';
-import { PurchasePageNYSw } from './pages/NYSweatPantsPurchasePage';
-import { PurchasePageTee } from './pages/NYTeePurchasePage';
+
 import { AddressFillingPage } from "./pages/AddressFillPage";
 import { PaymentPage } from "./pages/PaymentPage";
 import { DeliveryPage } from "./pages/DeliveryPage";
@@ -32,6 +25,10 @@ import AccountPageData from "./pages/AccountPageData";
 import OrderDetails from "./pages/admin/OrderDetails";
 import CourierDetails from "./pages/admin/CourierDetails";
 import ClientDetails from "./pages/admin/ClientDetails";
+import {OfferPage} from "./pages/shops/Offer";
+import {ShopPage} from "./pages/shops/ShopPage";
+import {CategoryPage} from "./pages/shops/CategoryPage";
+import {ProductPage} from "./pages/shops/ProductPage";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
